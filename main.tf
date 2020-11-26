118d4c87df6758df8be12ea4ebf073e581b69a86
tstanislawczyk07 <tstanislawczyk@pgs-soft.com>
2020-11-26 09:56:18 +0100

    1

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+ region = "us-west-2"
+}
+
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default = "10.1.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default = "10.1.0.0/24"
+}
+variable "availability_zone" {
+  description = "availability zone to create subnet"
+  default = "us-east-2a"
+}
+variable "public_key_path" {
+  description = "Public key path"
+  default = "~/.ssh/id_rsa.pub"
+}
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance"
+  default = "ami-0ac73f33a1888c64a"
+}
+variable "instance_type" {
+  description = "type for aws EC2 instance"
+  default = "t2.micro"
+}
+variable "environment_tag" {
+  description = "Environment tag"
+  default = "Production"
+}
+
